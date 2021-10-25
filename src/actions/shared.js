465811a0dd308c12ad7131bcea0aa27d1190c8d4
Ons Ghariani <ons@Onss-MacBook-Pro.local>
2021-10-24 18:53:19 -0700

    fixed authedUser bug

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,21 +1,19 @@
 import { getInitialData } from '../utils/_DATA.js'
 import { receiveUsers } from '../actions/users'
 import { receiveQuestions} from '../actions/questions'
-import { setAuthedUser } from '../actions/authedUser'
-//mport { showLoading, hideLoading } from 'react-redux-loading'
-const AUTHED_ID = 'tylermcginnis'
+
 
 export function handleInitilData(){
 
 return (dispatch) => {
-   // dispatch(showLoading())
+   
     return getInitialData()
     .then(
         ({users,questions}) =>{
             dispatch(receiveUsers(users))
             dispatch(receiveQuestions(questions))
-            dispatch(setAuthedUser(AUTHED_ID))
-           // dispatch(hideLoading())
+           
+           
         }
     )
 
